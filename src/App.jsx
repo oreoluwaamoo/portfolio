86ac005e9dccaf769a7625cddf183c2409d0bf3b
oreoluwaamoo <150631879+oreoluwaamoo@users.noreply.github.com>
2023-11-14 23:45:15 +0000

    My modification

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,9 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Oreoluwa Amoo",
+  title: "Network Analyst",
+  email: "oreoluwaamo4@gmail.com",
 };
 
 const primaryColor = "#4E567E";
